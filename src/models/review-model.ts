7b828039c0c58974db7a627accf1bdfc7d12b87e
RonGamarnik <rgam2002@gmail.com>
2024-07-23 13:41:29 +0300

    time in reviwe

--- src/models/review-model.ts
+++ src/models/review-model.ts
@@ -1,4 +1,5 @@
 import { Schema, model, Types } from "mongoose";
+import { formatDate } from "../helpers/formatDate";
 
 export interface IReview {
   _id: Types.ObjectId;
@@ -6,13 +7,17 @@ export interface IReview {
   business: Types.ObjectId;
   user: Types.ObjectId;
   likes: number;
+    time: string;
+
+  
 }
 
 const reviewSchema = new Schema<IReview>({
   content: { type: String, required: true },
   business: { type: Schema.Types.ObjectId, ref: "Business", required: true },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  likes:{type:Number, default:0}
+  likes: { type: Number, default: 0 },
+  time: { type: String, default: () => formatDate(new Date().getTime()) }, // Store formatted date
 });
 
 const Review = model<IReview>("Review", reviewSchema);

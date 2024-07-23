18856ca65ab4516ead4a878c3223f5e848f95049
Nevo <bitonnevo@gmail.com>
2024-07-23 13:15:16 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,3 @@
-// import express, { Application } from "express";
-// import { authRoutes } from "./routes/auth-routes";
-// import { businessRoute } from "./routes/business.route";
-// import cors from "cors";
-// import { connectDB } from "./config/db";
-
-// const PORT = process.env.PORT || 3000;
-
-// const app: Application = express();
-
-// async function main() {
-//   // Connect to database
-//   await connectDB();
-
-//   // Middleware
-//   app.use(express.json());
-//   // allow CORS for local development (for production, you should configure it properly)
-//   app.use(cors());
-
-//   // Routes
-
-//   app.use("/api/auth", authRoutes);
-//   app.use("/api/business", businessRoute);
-
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
-// }
-// main();
-
 import express, { Application, Express } from "express";
 import { authRoutes } from "./routes/auth-routes";
 import { businessRoute } from "./routes/business.route";
